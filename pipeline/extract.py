db16ace944123096b5398db7f91ed4cbe13dd2f4
Yacquub <b_yacquub@hotmail.co.uk>
2025-09-23 11:22:33 +0100

    changed docs

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,20 +1,27 @@
-import requests
+'''Extracts plant data from an api endpoint'''
 import json
 import logging
+import requests
 
 
-def loop_plants_api():
+def get_plant_by_id(plant_id):
+    """Gets plant data by id"""
+    url = f"https://sigma-labs-bot.herokuapp.com/api/plants/{plant_id}"
+    return requests.request(url=url, method='GET', timeout=5)
+
+
+def loop_plants_api(max_attempts: int = 1000, max_404_in_row: int = 5):
+    """Gets the plant data and appends it to a list, loops stops after n 404s"""
     count = 0
     plants = []
-    for i in range(1000):
-        num = i
-        url = f"https://sigma-labs-bot.herokuapp.com/api/plants/{num}"
-        req = requests.request(url=url, method='GET')
+    for i in range(max_attempts):
+        req = get_plant_by_id(i)
+
         if req.status_code == 404:
             count += 1
-            if count == 5:
+            if count == max_404_in_row:
                 logging.info(
-                    f"5 status code 404's in a row stopping loop! iteration {i}")
+                    f"{max_404_in_row} consecutive 404s. Stopping at iteration {i}.")
                 break
         if req.status_code == 200:
             count = 0
@@ -23,10 +30,12 @@ def loop_plants_api():
 
 
 def load_data_to_json(plants_data: list):
+    """Makes a folder and adds the plant data to it in json"""
     with open('plants.json', 'w') as f:
         json.dump(plants_data, f, indent=4)
 
 
 if __name__ == "__main__":
-    plants = loop_plants_api()
-    load_data_to_json(plants)
+    logging.basicConfig(level=logging.INFO)
+    plant_data = loop_plants_api()
+    load_data_to_json(plant_data)
