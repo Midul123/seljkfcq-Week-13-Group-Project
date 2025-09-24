2d9e1de0abb178ecfc62467175d455be4a85aef3
Yacquub <b_yacquub@hotmail.co.uk>
2025-09-24 10:23:29 +0100

    made ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,27 @@ resource "aws_s3_bucket" "project-s3" {
 
     force_destroy = true
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resource "aws_ecr_repository" "project-ecr" {
+  name                 = "c19-seljkfcq-ecr-tf"
+  image_tag_mutability = "MUTABLE"
+}
